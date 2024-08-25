d6ec56ced7d46108728f2c1c1ab49da0fa85f49e
m5x5 <michaelpeters5647@gmail.com>
2024-08-25 22:28:40 +0200

    add posthog out to improve onboarding experience

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,23 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { CSPostHogProvider } from "@/app/providers";
 
 export const metadata: Metadata = {
-  title: "Michael Peters - Web Developer Portfolio",
-  description:
-    "Ich bin ein Web Entwickler und fasziniere mich für Optimierung und Automatisierung. Ich habe Spaß an einer sauberen und guten Umsetzung.",
+	title: "Michael Peters - Web Developer Portfolio",
+	description:
+		"Ich bin ein Web Entwickler und fasziniere mich für Optimierung und Automatisierung. Ich habe Spaß an einer sauberen und guten Umsetzung.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="de">
-      <head>
-        <meta name="theme-color" content="#ffffff" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="de">
+			<head>
+				<meta name="theme-color" content="#ffffff" />
+			</head>
+			<CSPostHogProvider>
+				<body>{children}</body>
+			</CSPostHogProvider>
+		</html>
+	);
 }
